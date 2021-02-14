c938cda9419a3dcbd03aea5fbaa9aca8ba6f0f75
Syed Hassaan Ahmed <syedhassaan.103@gmail.com>
2021-02-14 17:36:28 +0500

    [main] remove empty spaces

--- Components/BadgesComponent.js
+++ Components/BadgesComponent.js
@@ -1,14 +1,13 @@
 import styles from '../styles/Badges.module.css'
 const BadgesComponent = (props) => {
-    const {files} = props || [];
+    const { files } = props || [];
     const languages = [...Object.keys(files).map(el => files[el].language)].filter((v, i, a) => a.indexOf(v) === i);
     return (
         <>
             {
-                languages.map(l => 
-                    {
-                        return <><span className={styles.badge}>{l}</span> {' '}</>
-                    })
+                languages.map(l => {
+                    return <><span className={styles.badge}>{l}</span> {' '}</>
+                })
             }
         </>
     );
